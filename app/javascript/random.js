5aeb2eef669afa5103439400437e7b6599b421d8
chiemechi <70624406+chiemechi@users.noreply.github.com>
2020-10-19 19:08:35 +0900

    修正

--- app/javascript/random.js
+++ app/javascript/random.js
@@ -27,3 +27,49 @@ function stop(){
   })
 }
 window.addEventListener('load',stop)
+
+
+// ⭐️誰が話すか
+
+    //参加者一覧
+    let num_1 = [];
+    window.onload = function() {
+    const addNameButton = document.getElementById('add-button')
+    addNameButton.addEventListener('click', () => {
+    const input = document.getElementById('add-name');
+    const li =document.createElement('li');
+    li.innerText = input.value;
+    const ul =document.querySelector('ul');
+    ul.appendChild(li);
+    let name = input.value
+     num_1.push(name)
+    });
+    }
+  
+    // ルーレットを開始
+
+    function talker_a() {
+      const start = document.getElementById("talker1")
+      talker1.addEventListener('click', function() {
+
+      talker_roulette = setInterval(function() {
+       document.getElementById("talker1").disabled = true;
+      var idx = Math.floor( Math.random() * num_1.length );
+      document.getElementById("talker_roulette").innerHTML = num_1[idx];
+      }) 
+       }, 100);
+        }
+        window.addEventListener('load',talker_a)
+
+
+      // ルーレットを停止
+        function talker_b(){
+          const start = document.getElementById("talker1_2")
+          talker1_2.addEventListener('click', function() {
+          document.getElementById("talker1").disabled = false;
+          if(talker_roulette) {
+            clearInterval(talker_roulette);
+          }
+        }) 
+        }   
+        window.addEventListener('load',talker_b)
